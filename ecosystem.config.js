d990f07faeb1136ba1c439e92e34c0aa41634495
Byronwezvo <byron.wezvo@outrisk.co.zw>
2025-10-23 12:59:16 +0000

    server changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'Outrisk-',
+      name: 'OUTRISK-LEADS-APPLICATION',
       script: './node_modules/next/dist/bin/next',
       args: 'start',
       cwd: '/var/outrisk/Outleads',
